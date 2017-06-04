76cf4d042920e42702d537139e414e78f62b72d1
Sujan Deshpande <sdeshpande@mobileiron.com>
2017-06-04 23:50:04 +0530

    regex

--- Main.go
+++ Main.go
@@ -6,6 +6,8 @@ import (
 	"io/ioutil"
 	"movie/Structures"
 	"movie/fileUtils"
+	"regexp"
+	"strconv"
 	"strings"
 )
 
@@ -20,14 +22,37 @@ func main() {
 
 func assignNewName(movies *Structures.MovieInfo) {
 	for _, element := range movies.Movies {
-		str := strings.Split(element.Name, " ")
+		var str []string
+		re := regexp.MustCompile("(.*\\.)+.*")
+		if len(re.FindString(element.Name)) > 0 {
+			str = strings.Split(element.Name, ".")
+		} else {
+			str = strings.Split(element.Name, " ")
+		}
 		for index, stringer := range str {
-			output := fmt.Sprintf("%d) %s",index,stringer )
+			output := fmt.Sprintf("%d) %s", index, stringer)
 			fmt.Println(output)
 
 		}
-		var input string
-		fmt.Scanln(&input)
-		fmt.Print(input)
+		var inputStr string
+		fmt.Scanln(&inputStr)
+		//
+		// input := strings.Split(inputStr, " ")
+
+		//convertStringToIntArray(input)
 	}
+}
+
+func convertStringToIntArray(strings []string) {
+
+	var t2 = []int{}
+	for _, i := range strings {
+		fmt.Println(i)
+		j, err := strconv.Atoi(i)
+		if err != nil {
+			panic(err)
+		}
+		t2 = append(t2, j)
+	}
+	fmt.Println(t2)
 }
