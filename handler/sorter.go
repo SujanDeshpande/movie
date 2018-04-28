fb11fddd67ad736081a73498c8ba1f50a09e9fa7
Sujan Deshpande <sdeshpande@mobileiron.com>
2018-04-28 21:24:05 +0530

    Added DB info

--- handler/sorter.go
+++ handler/sorter.go
@@ -1,6 +1,8 @@
 package Handler
 
 import (
+	"encoding/json"
+	"errors"
 	"io/ioutil"
 	"movie/MovieDB"
 	"movie/Utils"
@@ -8,6 +10,7 @@ import (
 	"os"
 	"strconv"
 
+	"github.com/boltdb/bolt"
 	log "github.com/sirupsen/logrus"
 )
 
@@ -20,15 +23,69 @@ func SortHandler(db MovieDB.DB) http.Handler {
 		infoChan := getFileInfo(srcFolder)
 		destChan := createDestination(infoChan, destFolder)
 		moveChan := moveFile(destChan, srcFolder)
-		for filer := range moveChan {
+		writeChan := writeToDB(moveChan, db)
+
+		for filer := range writeChan {
 			log.Info("processed" + filer.info.Name())
+			filers := getFileInfoFromDB(db)
+			for i, _ := range filers {
+				log.Info("Retrieved" + strconv.Itoa(i))
+			}
+
 		}
+
 	})
+}
+
+func writeToDB(inChan <-chan filer, db MovieDB.DB) <-chan filer {
+	outChan := make(chan filer)
+	go func() {
+		defer close(outChan)
+		for filer := range inChan {
+			log.Info("writing")
+			addFileInfoToDB(filer, db)
+			outChan <- filer
+		}
+	}()
+	return outChan
 }
 
 type filer struct {
 	info        os.FileInfo
 	destination string
+}
+
+// AddFileInfoToDB Adds a single Movie
+func addFileInfoToDB(filer filer, db MovieDB.DB) {
+	db.Bolted.Update(func(tx *bolt.Tx) error {
+		bucket := tx.Bucket([]byte(Utils.GetDatabaseConfig().Bucket))
+		if bucket == nil {
+			berr := errors.New("Bucket not found")
+			log.WithError(berr).Error("Bucket not found")
+		}
+		jvalue, _ := json.Marshal(filer)
+		perr := bucket.Put([]byte(filer.info.Name()), jvalue)
+		if perr != nil {
+			log.WithError(perr).Error("Could not persist movie")
+		}
+		return nil
+	})
+}
+
+//getFileInfoFromDB - read movie based on id
+func getFileInfoFromDB(db MovieDB.DB) []filer {
+	var filers []filer
+	db.Bolted.View(func(tx *bolt.Tx) error {
+		bucket := tx.Bucket([]byte(Utils.GetDatabaseConfig().Bucket))
+		cursor := bucket.Cursor()
+		for k, v := cursor.First(); k != nil; k, v = cursor.Next() {
+			jvalue := filer{}
+			json.Unmarshal(v, &jvalue)
+			filers = append(filers, jvalue)
+		}
+		return nil
+	})
+	return filers
 }
 
 func getFileInfo(srcFolder string) <-chan os.FileInfo {
